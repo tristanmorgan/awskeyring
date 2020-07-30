47a832304ae3452babff93a0d244ee580fd93bf8
Tristan Morgan <tristanmorgan@users.noreply.github.com>
2020-07-30 13:49:29 +1000

    Add SimpleCov reports.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+SimpleCov.start do
+  minimum_coverage 85
+  add_filter '/spec/'
+end
+
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'awskeyring'
